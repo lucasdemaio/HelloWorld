b15ae918d8aebee68d9e96c614ff31c00ffeef12
lucasdemaio <lucas.demaio25@hotmail.com>
2021-08-24 16:49:52 -0300

    Agregar archivos de proyecto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            Console.WriteLine("Presione una tecla para finalizar");
+            Console.ReadKey();
+
+
+        }
+    }
+}
